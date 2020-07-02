e9a902f1097c7b3c8ebeb34b14e9f71c9fcfebd4
Neomachina <46264653+Neomachina@users.noreply.github.com>
2020-07-02 11:32:34 +0200

    Add files via upload
    initial commit

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,16 @@
+package de.hfu;
+import java.util.Scanner; 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner in = new Scanner(System.in); 
+  
+        String s = in.nextLine(); 
+        System.out.println(s.toUpperCase()); 			
+    }
+}
